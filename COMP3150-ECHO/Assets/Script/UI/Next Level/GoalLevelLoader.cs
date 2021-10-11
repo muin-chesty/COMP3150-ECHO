3c32313fcdcca8fad5281e154d8b854a54940ebe
WilliamXu135 <61869357+WilliamXu135@users.noreply.github.com>
2021-10-11 20:41:48 +1100

    Completed Tutorial Level

---
+++ COMP3150-ECHO/Assets/Script/UI/Next Level/GoalLevelLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalLevelLoader : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Activate End level Panel
+    }
+}
--- COMP3150-ECHO/Assets/Script/UI/Tutorial.cs
+++ COMP3150-ECHO/Assets/Script/UI/Tutorial.cs
@@ -1,30 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class Tutorial : MonoBehaviour
 {
-    private GameObject textObject;
-    private Text textPanel;
+    private TutorialUIManager tutorialManager;
+    private bool activated;
     public string customText;
 
-    // Start is called before the first frame update
     void Start()
     {
-        textObject = GameObject.Find("Tutorial Text Box");
-        textPanel = textObject.GetComponent<Text>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(textPanel.name);
+        tutorialManager = FindObjectOfType<TutorialUIManager>();
+        activated = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        textPanel.text = customText;
+        if (!activated)
+        {
+            tutorialManager.UpdateTutorialText(customText);
+            activated = true;
+        }
     }
 }
