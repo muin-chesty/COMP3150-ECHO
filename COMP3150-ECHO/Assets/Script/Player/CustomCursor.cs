3e20d2bd45c3a64c2ca764119619cd7259816e04
Muin Chesty <53714737+muin-chesty@users.noreply.github.com>
2021-10-22 22:23:59 +1100

    UI for Cursor fixed for both levels!

--- COMP3150-ECHO/Assets/Script/Player/CustomCursor.cs
+++ COMP3150-ECHO/Assets/Script/Player/CustomCursor.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CustomCursor : MonoBehaviour
 {
+    public GameObject pausePanel;
+    public GameObject completePanel;
     void Start()
     {
         Cursor.visible = false;
@@ -10,7 +12,17 @@ public class CustomCursor : MonoBehaviour
     
     void Update()
     {
-        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-       transform.position =  pos;
+
+        if (!pausePanel.activeSelf && !completePanel.activeSelf)
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = pos;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
+
     }
 }
