3b772eacc221db22628b43f7aa0a66365eb54231
WilliamXu135 <61869357+WilliamXu135@users.noreply.github.com>
2021-10-27 16:53:45 +1100

    Improved Tutorial playablilty

--- COMP3150-ECHO/Assets/Script/UI/TutorialUIManager.cs
+++ COMP3150-ECHO/Assets/Script/UI/TutorialUIManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class TutorialUIManager : MonoBehaviour
 {
     private GameObject textObject;
     private Text textPanel;
+
+    [SerializeField]
+    private Image textBackground;
+
+    public float textColourTime;
+    private float textColourTimer;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +22,29 @@ public class TutorialUIManager : MonoBehaviour
         textObject = GameObject.Find("Tutorial Text Box");
         textPanel = textObject.GetComponent<Text>();
         textPanel.text = "";
+        textBackground.color = Color.gray;
+        textPanel.color = Color.white;
+
+        textColourTimer = textColourTime;
+    }
+
+    private void Update()
+    {
+        textColourTimer -= Time.deltaTime;
+        if (textColourTimer <= 0)
+        {
+            textBackground.color = Color.gray;
+            textPanel.color = Color.white;
+        }
+
+
     }
 
     public void UpdateTutorialText(string customText)
     {
         textPanel.text = customText;
+        textColourTimer = textColourTime;
+        textBackground.color = Color.white;
+        textPanel.color = Color.black;
     }
 }
