6d29d802a64bff11212c2415763e17da2433de7e
WilliamXu135 <61869357+WilliamXu135@users.noreply.github.com>
2021-10-27 17:11:36 +1100

    Recovered lost work

--- COMP3150-ECHO/Assets/Script/UI/RestartGame.cs
+++ COMP3150-ECHO/Assets/Script/UI/RestartGame.cs
@@ -3,10 +3,8 @@ using UnityEngine;
 
 public class RestartGame : MonoBehaviour
 {
-    public HealthManager hp;
     public void RestartWholeGame()
     {
-        hp.SetHealth(5);
         TransitionPlayer.instance = null;   // TO RESET THE START TRANSITION
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
